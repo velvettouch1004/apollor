1f59d5623e8c81b21a981b901054c9e627c8e24d
velvettouch1004 <velvettouch1004@gmail.com>
2022-12-13 11:01:24 +0100

    sbi_key in apollor

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' KvK SBI codes
+#'
+#'
+#' @format A data frame with 1434 rows and 7 variables:
+#' \describe{
+#'   \item{sbi_code_txt}{}
+#'   \item{sbi_code_int}{}
+#'   \item{sbi_omschrijving}{}
+#'   \item{sbi_code_main}{}
+#'   \item{sbi_code_sub_1}{}
+#'   \item{sbi_code_sub_2}{}
+#'   \item{sbi_code_type}{}
+#' }
+"sbi_key"
